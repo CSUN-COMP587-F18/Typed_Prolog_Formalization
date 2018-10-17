ea4fdfe755f867fa8a7a3cdc6728e39464789155
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2018-10-17 11:46:07 -0700

    Arbitrary ADT proof of concept

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import Generator (generate)
+import Generator (generatePrologFile)
 import System.Environment
 
 
 main :: IO ()
 main = do
     args <- getArgs
-    writeFile (head args) $ generate "true"
+    writeFile (head args) $ generatePrologFile "true"
 
