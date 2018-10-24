18b92f192f60a53a61384f851b9576d55a1ecb0f
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2018-10-23 18:52:20 -0700

    added expressions and body_is rules

--- src/Generator.hs
+++ src/Generator.hs
@@ -7,42 +7,85 @@ import Test.QuickCheck
 import Test.QuickCheck.Arbitrary.ADT
 
 
-data Body = Is Term Term deriving (Generic)
-instance Show Body where
-    show (Is t1 t2) = show t1 ++ " is "++ show t2
+data Exp = 
+    IntExp Int | 
+    VarExp String |
+    ExpBOp Exp BinOp Exp
+    deriving (Generic)
+    
+data Body = Is Elhs Exp deriving (Generic)
 
-instance Arbitrary Body where
-  arbitrary = genericArbitrary
+data BinOp = Plus | Minus | Div deriving (Generic)
 
-instance ToADTArbitrary Body
+data UnOp = Msb | Abs | Truncate deriving (Generic)
 
+data Elhs = IntElhs Int | VarElhs String deriving (Generic)
+        
 data Term = IntTerm Int | VarTerm String deriving (Generic)
+
+
+instance Show Exp where
+    show (IntExp i) = show i
+    show (VarExp str) = str
+    show (ExpBOp e1 bop e2) = show e1 ++ show bop ++ show e2
+    
+instance Show Body where
+    show (Is elhs exp) = show elhs ++ " is " ++ show exp
+    
+instance Show BinOp where
+    show (Plus) = "+"
+    show (Minus) = "-"
+    show (Div) = "/"
+
+instance Show Elhs where
+    show (IntElhs int) = show int
+    show (VarElhs str) = str
+
 instance Show Term where
     show (IntTerm int) = show int
     show (VarTerm str) = str
 
+
+instance Arbitrary Exp where
+  arbitrary = genericArbitrary
+
+instance Arbitrary Body where
+  arbitrary = genericArbitrary
+
+instance Arbitrary BinOp where
+  arbitrary = genericArbitrary
+
+instance Arbitrary Elhs where
+  arbitrary = genericArbitrary
+
 instance Arbitrary Term where
   arbitrary = genericArbitrary
+
+instance ToADTArbitrary Exp
+
+instance ToADTArbitrary Body
+
+instance ToADTArbitrary BinOp
+
+instance ToADTArbitrary Elhs
 
 instance ToADTArbitrary Term
 
 extractAdt :: ADTArbitrarySingleton a -> a
 extractAdt (ADTArbitrarySingleton _ _ (ConstructorArbitraryPair _ adt)) = adt
 
-generateTerm :: IO Term
+generateTerm ::IO Term
 generateTerm = do
     term <- generate (toADTArbitrarySingleton (Proxy :: Proxy Term))
     return $ extractAdt term
 
-generateBody :: IO Body
+generateBody ::IO Body
 generateBody = do
-    body <- generate (toADTArbitrarySingleton (Proxy :: Proxy Body))
-    return $ extractAdt body
+    term <- generate (toADTArbitrarySingleton (Proxy :: Proxy Body))
+    return $ extractAdt term
         
 generatePrologFile :: IO String
 generatePrologFile = do 
-    body <- generateTerm
+    body <- generateBody
     return $ "clausedef(test, [], []). \n\
       \test :- \n\t"++ show body ++"."
-
-
